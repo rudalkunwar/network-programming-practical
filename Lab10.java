d3c11a548d46ed567250e8fa57451fae36a869f6
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-08-22 21:20:24 +0545

    lab10

---
+++ Lab10.java
@@ -0,0 +1,20 @@
+import java.net.*;
+import java.io.*;
+
+public class URLConnectionExample {
+    public static void main(String[] args) throws MalformedURLException,IOException {
+    
+            // Specify the URL to connect to
+            URL url = new URL("https://rudalkunwar.com.np"); // Replace with any valid URL
+
+            // Open a connection to the URL
+            URLConnection uc = url.openConnection();
+
+            // Display some information about the connection
+            System.out.println("URL: " + uc.getURL());
+            System.out.println("Content Type: " + uc.getContentType());
+            System.out.println("Content Length: " + uc.getContentLength());
+            System.out.println("Last Modified: " + uc.getLastModified());
+            System.out.println("Expiration: " + uc.getExpiration());
+    }
+}
---
+++ Lab9.java
@@ -0,0 +1,33 @@
+// Write a simple program for implementation of cookiepolicy
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Lab9 {
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        URL url = new URL("https://www.rudalkunwar.com.np");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        // Set the cookie if not an .edu domain
+        String mycookie = "name=rudal";
+        con.setRequestProperty("Cookie", mycookie);
+
+        // Connect to the server
+        con.connect();
+
+        // Get the response headers
+        Map<String, List<String>> headerFields = con.getHeaderFields();
+        
+        List<String> cookieHeader = headerFields.get("Set-Cookie");
+
+        // Check if the Set-Cookie header is present
+        if (cookieHeader != null) {
+            for (String cookie : cookieHeader) {
+                System.out.println("Set-Cookie: " + cookie);
+            }
+        } else {
+            System.out.println("No Set-Cookie header found.");
+        }
+    }
+}
